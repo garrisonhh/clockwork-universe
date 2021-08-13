ca1b88d7105b2ccbcf72201497946ad91fabbfd9
garrisonhh <garrisonhh@pm.me>
2021-08-13 13:57:21 -0400

    remove libs before submoduling

--- external/glad/obj/glad/debug/glad.d
+++
@@ -1,3 +0,0 @@
-../../../glad/obj/glad/debug/glad.o: ../../../glad/src/glad.c \
- include/glad/glad.h
-include/glad/glad.h:
--- external/glad/obj/glad/release/glad.d
+++
@@ -1,2 +0,0 @@
-obj/glad/release/glad.o: src/glad.c include/glad/glad.h
-include/glad/glad.h:
